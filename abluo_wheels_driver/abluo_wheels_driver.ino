22dfabe0eeaafe050ce819f9c6aba28d351824d7
Rishab Patwari <patwariri@gmail.com>
2022-06-02 01:48:17 +0800

    feat: added wheel direction control

--- abluo_wheels_driver/abluo_wheels_driver.ino
+++ abluo_wheels_driver/abluo_wheels_driver.ino
@@ -32,20 +32,43 @@
 
 // DC Controller 2 - Motor 1
 #define M2A_EN 50
-#define M2_IN1 48
-#define M2_IN2 46
+#define M2_IN1 46
+#define M2_IN2 48
 // DC Controller 2 - Motor 2
-// #define M2B_EN 51
-// #define M2_IN3 47
-// #define M2_IN4 49
-#define M2B_EN 9
-#define M2_IN3 10
-#define M2_IN4 11
+#define M2B_EN 51
+#define M2_IN3 49
+#define M2_IN4 47
+// #define M2B_EN 9
+// #define M2_IN3 10
+// #define M2_IN4 11
 
 // Servo Controller Pin
 #define SPIN 2
+#define SPEED_LIMIT 25
 
 bool settingUp = true;
+enum WHEELS_INDEX
+{
+  FRONT_LEFT = 0,
+  FRONT_RIGHT = 1,
+  BACK_LEFT = 2,
+  BACK_RIGHT = 3
+};
+enum DIRECTION
+{
+  FORWARD = 0,
+  BACKWARD = 1,
+  LEFT = 2,
+  RIGHT = 3,
+  FORWARD_RIGHT = 4,
+  FORWARD_LEFT = 5,
+  BACKWARD_RIGHT = 6,
+  BACKWARD_LEFT = 7,
+  CLOCKWISE = 8,
+  ANTI_CLOCKWISE = 9
+};
+unsigned long motion_duration;
+unsigned long motion_start_millis;
 
 typedef void (*functiontype)();
 unsigned long currentMicros = micros();
@@ -53,14 +76,12 @@ unsigned long currentMicros = micros();
 // Controller M2: Motor PWM Controller
 // Frequency: (1us x 58 Count = 58 us Period / 17.2 kHz)
 const byte motorMicroInterval = 1;
-const byte motorPwmMax = 100;
+const byte motorPwmMax = 58;
 const byte motorPinsCount = 4;
 const byte motorPwmPins[motorPinsCount] =
     {
         M1A_EN,
-        M1B_EN
-        , M2A_EN, M2B_EN
-};
+        M1B_EN, M2A_EN, M2B_EN};
 PWM_PROCESSING pwmMotorsController;
 
 // Motor Data Structures
@@ -84,8 +105,7 @@ const byte digitalPins[digitalPinCount] = {
     M2_IN1,
     M2_IN2,
     M2_IN3,
-    M2_IN4
-};
+    M2_IN4};
 
 // Millis Timer
 unsigned long currentMillis = millis();
@@ -181,22 +201,15 @@ void setupMotors()
   }
 }
 
-void handleDcMotor(int payload[], functiontype setDir_1, functiontype setDir_2, functiontype brake)
+void handleDcMotor(int motorIndex, int status, int direction, int speed, functiontype setDir_1, functiontype setDir_2, functiontype brake)
 {
-  int motorIndex = payload[0] - 1;
-  int status = payload[1];
-  int direction = payload[2];
-  int speed = payload[3];
-
   myMotors[motorIndex].status = status;
   myMotors[motorIndex].speed = speed;
   myMotors[motorIndex].direction = direction;
   if (myMotors[motorIndex].status)
   {
-      myMotors[motorIndex].direction == 1 ? setDir_1() : setDir_2();
-    pwmMotorsController.updatePinPwmDutyCycle(motorIndex, speed, pwmMaxMultiplier);
-    // pwmMotorsController.updatePinPwmValue(motorIndex, speed);
-    // analogWrite(pwmEnPinValue, speed*0.01*255);
+    myMotors[motorIndex].direction == 1 ? setDir_1() : setDir_2();
+    pwmMotorsController.updatePinPwmDutyCycle(motorIndex, speed);
   }
   else
   {
@@ -205,36 +218,131 @@ void handleDcMotor(int payload[], functiontype setDir_1, functiontype setDir_2, 
   Serial.println("[DONE]");
 }
 
-void handleServo(int status, functiontype onServo, functiontype offServo)
+void stopAllMotors()
 {
-  status == 1 ? onServo() : offServo();
-  myMotors[0].status = status;
+  for (int i = 0; i < MotorCount; i++)
+  {
+    myMotors[i].status = 0;
+  }
+  brake_M1A();
+  brake_M1B();
+  brake_M2A();
+  brake_M2B();
+}
+
+void handleMotorMotion()
+{
+  if (motion_duration > 0)
+  {
+    currentMillis = millis();
+    if (currentMillis - motion_start_millis > motion_duration)
+    {
+      char buffer[40];
+      sprintf(buffer, "[Motion Completed] - Duration = %d", motion_duration);
+      motion_duration = 0;
+      stopAllMotors();
+      Serial.println(buffer);
+    }
+  }
 }
 
 void handleUpdate()
 {
   int motorId = payload[0];
+  int motorIndex = payload[0] - 1;
+  int status = payload[1];
+  int direction = payload[2];
+  int speed = payload[3];
+  if (speed > SPEED_LIMIT)
+  {
+    speed = SPEED_LIMIT;
+  }
   switch (motorId)
   {
   case 1: // M1_A - Front Left
   {
-    handleDcMotor(payload, set_M1A_D1, set_M1A_D2, brake_M1A);
+    handleDcMotor(motorIndex, status, direction, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
     break;
   }
   case 2: // M1_B - Front Right
   {
 
-    handleDcMotor(payload, set_M1B_D1, set_M1B_D2, brake_M1B);
+    handleDcMotor(motorIndex, status, direction, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
     break;
   }
-  case 3: // M2_A - Back Right
+  case 3: // M2_A - Back Left
   {
-    handleDcMotor(payload, set_M2A_D1, set_M2A_D2, brake_M2A);
+    handleDcMotor(motorIndex, status, direction, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
     break;
   }
-  case 4: // M2_B - Back Left
+  case 4: // M2_B - Back Right
   {
-    handleDcMotor(payload, set_M2B_D1, set_M2B_D2, brake_M2B);
+    handleDcMotor(motorIndex, status, direction, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    break;
+  }
+  case 5: // Move Forward or Back
+  {
+    int direction = payload[1];
+    int duration = payload[2];
+    int speed = payload[3];
+    motion_duration = duration;
+    motion_start_millis = millis();
+    if (speed > SPEED_LIMIT)
+    {
+      speed = SPEED_LIMIT;
+    }
+
+    if (direction == DIRECTION::FORWARD)
+    { // Forward
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::FORWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::FORWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::FORWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::FORWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    if (direction == DIRECTION::BACKWARD)
+    { // BACKWARD
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::BACKWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::BACKWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    if (direction == DIRECTION::RIGHT)
+    { // RIGHT
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::FORWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::BACKWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::FORWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    if (direction == DIRECTION::LEFT)
+    { // LEFT
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::BACKWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::FORWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::FORWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    if (direction == DIRECTION::CLOCKWISE)
+    {
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::FORWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::FORWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::BACKWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    if (direction == DIRECTION::ANTI_CLOCKWISE)
+    {
+      handleDcMotor(WHEELS_INDEX::FRONT_LEFT, 1, DIRECTION::BACKWARD, speed, set_M1A_D1, set_M1A_D2, brake_M1A);
+      handleDcMotor(WHEELS_INDEX::FRONT_RIGHT, 1, DIRECTION::FORWARD, speed, set_M1B_D1, set_M1B_D2, brake_M1B);
+      handleDcMotor(WHEELS_INDEX::BACK_LEFT, 1, DIRECTION::BACKWARD, speed, set_M2A_D1, set_M2A_D2, brake_M2A);
+      handleDcMotor(WHEELS_INDEX::BACK_RIGHT, 1, DIRECTION::FORWARD, speed, set_M2B_D1, set_M2B_D2, brake_M2B);
+    }
+    break;
+  }
+  case 6:
+  {
+    int direction = payload[1];
+    int duration = payload[2];
+    int speed = payload[3];
+    stopAllMotors();
+    Serial.println("[STOPPED ALL MOTORS]");
     break;
   }
   default:
@@ -267,5 +375,6 @@ void loop()
     recvWithEndMarker();
     processNewData();
     pwmMotorsController.handlePWM();
+    handleMotorMotion();
   }
 }
